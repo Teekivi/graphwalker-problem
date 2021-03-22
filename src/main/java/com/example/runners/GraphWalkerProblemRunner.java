a4b56453bf18f33cc4fd0a2b9edc618371991aa9
Kristian Karl <kristian.hermann.karl@gmail.com>
2021-03-22 09:44:35 +0100

    Suggested solution

--- src/main/java/com/example/runners/GraphWalkerProblemRunner.java
+++ src/main/java/com/example/runners/GraphWalkerProblemRunner.java
@@ -1,11 +1,13 @@
 package com.example.runners;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.graphwalker.core.condition.ReachedVertex;
 import org.graphwalker.core.condition.TimeDuration;
 import org.graphwalker.core.generator.AStarPath;
 import org.graphwalker.core.generator.RandomPath;
+import org.graphwalker.core.machine.Context;
 import org.graphwalker.core.model.Vertex;
 import org.graphwalker.java.test.TestExecutor;
 
@@ -13,12 +15,20 @@ import com.example.modelimplementations.FirstViewTest;
 import com.example.modelimplementations.SecondViewTest;
 
 public class GraphWalkerProblemRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         TestExecutor executor = new TestExecutor(
-                new FirstViewTest().setPathGenerator(new AStarPath(new ReachedVertex("v_SecondView")))
-                        .setCurrentElement(new Vertex().build()),
-                new SecondViewTest().setPathGenerator(new RandomPath(new TimeDuration(10, TimeUnit.SECONDS)))
+                FirstViewTest.class,
+                SecondViewTest.class
         );
+
+        for (Context context : executor.getMachine().getContexts()) {
+            if (context instanceof FirstViewTest) {
+                context.setPathGenerator(new AStarPath(new ReachedVertex("v_SecondView")));
+            } else if( context instanceof SecondViewTest) {
+                context.setPathGenerator(new RandomPath(new TimeDuration(10, TimeUnit.SECONDS)));
+            }
+        }
+
         executor.execute(true);
     }
 }
